a8d19ed01bc9383c8951cc85d760b2bc85e27323
Louis Liu <louixliu@outlook.com>
2023-10-09 21:01:43 +0800

    feat: add a link to GitHub repo on about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About UniHub</h1>
+    Source code for UniHub can be found on <a href="https://github.com/louix33/UniHub">GitHub</a>.
   </div>
 </template>
